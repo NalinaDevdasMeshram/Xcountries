09130ad6f43a7411c3d72b5312860baf49bca1f1
nalina <naleenameshram85@gmail.com>
2024-07-17 19:50:02 +0530

    done

--- country/src/Components/Country/Xcountries.jsx
+++ country/src/Components/Country/Xcountries.jsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react';
-import styles from '../Country/Xcountries.module.css'
-const Xcountries =()=>{
-     const [country, setCountry] = useState([]);
-    
-    const handleFlagFetch =async()=>{
-        try{
-            const response = await fetch('https://xcountries-backend.azurewebsites.net/all');
-            const flagData = await response.json();
-             console.log("flagdata is::", flagData)
-             setCountry(flagData)  
-        }
-        catch(e){
-            console.error("Error fetching data::",e.message)
-        }
-      
-    }
-    useEffect(()=>{
-        handleFlagFetch();
-    },[])
- return (
-    <div className={styles.container}>
-        {
-         country.map((data)=>{
-            // console.log('data id', data.abbr)
+import { useEffect, useState } from "react";
+import styles from './Xcountries.module.css';
+
+const Xcountries = () => {
+  const [country, setCountry] = useState([]);
+  const fetchingData = async()=>{
+    try{
+      const response = await fetch(` https://xcountries-backend.azurewebsites.net/all`)
+       const responseResult = await response.json();
+        // console.log('fetch country data', responseResult)
+        setCountry(responseResult);
+   }
+   catch(e){
+        console.error(e.message)
+   }
+  }
+ 
+  useEffect(()=>{
+    fetchingData();
+  },[])
+  return(
+    <div className={styles.Xcontainer}>
+      {
+        country.map((item)=>{
           return(
-            <div className={styles.xcountry} key={data.abbr}> 
-              <img className={styles.imgSrc}  src={data.flag} alt={data.name}/>
-              <h4>{data.name}</h4>
+            <div className={styles.xcountry} key={item.abbr}>
+              <img className={styles.imgContainer}src={item.flag} alt={item.name}/>
+              <h4>{item.name}</h4>
             </div>
-              );
-            })
-         }
-       </div>
- )
+          )
+        })
+      }
+    </div>
+  )
 }
+
 export default Xcountries
